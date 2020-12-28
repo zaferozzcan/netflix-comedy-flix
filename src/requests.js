35b986e371a23500961d2269d017b0bbeb1373cd
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-28 17:42:16 -0500

    add initial files to comedy-flix

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const api_key = process.env.REACT_APP_TMDM_API_KEY;
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${api_key}&language=en-US`,
+  fetchNetflixOriginals: `discover/tv?api_key=${api_key}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${api_key}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${api_key}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${api_key}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${api_key}&with_genres=27`,
+  fetchRomenceMovies: `/discover/movie?api_key=${api_key}&with_genres=10749`,
+  fetchDocumentariesMovies: `/discover/movie?api_key=${api_key}&with_genres=99`,
+};
+
+export default requests;
