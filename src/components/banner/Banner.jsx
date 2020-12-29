917f1a1adc08f4625a6bca7785a7aeb076d772b9
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-29 15:16:11 -0500

    set header

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import "../../styles/Banner.css";
 import axiosInstance from "../../axios";
 import requests from "../../requests";
-import YouTube from "react-youtube";
-import movieTrailer from "movie-trailer";
 
 export default function Banner() {
   const [movie, setMovie] = useState([]);
@@ -22,12 +20,6 @@ export default function Banner() {
   }, []);
   //   console.log("randim movie", movie);
 
-  const getMovieId = () => {};
-
-  const handleClick = () => {
-    trailer = true;
-  };
-
   return (
     <header
       className="banner"
@@ -42,15 +34,11 @@ export default function Banner() {
         </h1>
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
-          <button onClick={handleClick} className="banner_button">
-            My List
-          </button>
+          <button className="banner_button">My List</button>
         </div>
         <h1 className="banner_decs">{movie?.overview}</h1>
         <div className="banner_fadeBottom"></div>
       </div>
-
-      <YouTube videoId={} opts={} />
     </header>
   );
 }
